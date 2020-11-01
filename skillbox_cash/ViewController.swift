d5c315d2820f71a3fd23f9092010b62b1a6c9f57
Lynxy <anton@timonenkov.ru>
2020-11-01 12:08:00 +0200

    Initial Commit

---
+++ skillbox_cash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  skillbox_cash
+//
+//  Created by Антон Тимоненков on 01.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
